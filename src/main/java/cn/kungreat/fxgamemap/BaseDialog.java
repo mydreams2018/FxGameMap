c37b4e4834c16d4643aa35fac75c923bee3d1d8e
mydreams2018 <mydreams2018@outlook.com>
2024-05-01 20:41:09 +0800

    单个大图片选择 弹窗

--- src/main/java/cn/kungreat/fxgamemap/BaseDialog.java
+++ src/main/java/cn/kungreat/fxgamemap/BaseDialog.java
@@ -1,11 +1,11 @@
 package cn.kungreat.fxgamemap;
 
 import javafx.scene.Node;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import java.io.File;
 
 /* 泛型影响
  * final ObjectProperty<Callback<ButtonType,R>> resultConverter
@@ -31,6 +31,26 @@ public class BaseDialog {
     public static final TextField TEXT_MAP = BaseDialog.getTextField("请输入名称");
     public static final TextField TEXT_MAP_WIDTH = BaseDialog.getTextField("请输入宽度");
     public static final TextField TEXT_MAP_HEIGHT = BaseDialog.getTextField("请输入高度");
+
+    //SegmentResourceImagesDialog
+    public static final TextField SRID_NAME = BaseDialog.getTextField("请输入资源名称");
+    public static final TextField SRID_PATH = BaseDialog.getTextField("请选择资源文件");
+    public static final Button SRID_BTN_CHOOSE = new Button("点击选择文件");
+    public static final TextField SRID_WIDTH = BaseDialog.getTextField("请输入分割宽度");
+    public static final TextField SRID_HEIGHT = BaseDialog.getTextField("请输入分割高度");
+    public static final TextField SRID_MARGIN = BaseDialog.getTextField("请输入外边距");
+    public static final TextField SRID_PADDING = BaseDialog.getTextField("请输入内边距");
+    public static final ButtonType APPLY_SRID = new ButtonType("Apply", ButtonBar.ButtonData.OK_DONE);
+    public static final ButtonType CANCEL_SRID = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+    static {
+        SRID_BTN_CHOOSE.setOnAction(event -> {
+            File selectedFile = ResourceTab.FILE_CHOOSER.showOpenDialog(RootApplication.mainStage);
+            if (selectedFile != null) {
+                SRID_PATH.setText(selectedFile.toString());
+            }
+        });
+    }
 
     public static Dialog<String> getDialog(String title, String header, String content, Node graphic, ButtonType... buttonType) {
         Dialog<String> dialog = new Dialog<>();
@@ -59,5 +79,29 @@ public class BaseDialog {
         VBox vb = new VBox();
         vb.getChildren().addAll(TEXT_AREA, TEXT_AREAX, TEXT_AREAY);
         return vb;
+    }
+
+    /*
+     * 打开单个大的资源图片 进行分割
+     * */
+    public static Dialog<String> getSegmentResourceImagesDialog() {
+        Dialog<String> dialog = new Dialog<>();
+        dialog.setTitle("分割图片");
+        dialog.setHeaderText("属性设置");
+        dialog.setResizable(false);
+        VBox vBox = new VBox(10);
+        //只读文本输入框
+        SRID_PATH.setDisable(true);
+//        SRID_PATH.setEditable(false);不可编辑
+        HBox hBoxInnerOne = new HBox(10);
+        hBoxInnerOne.getChildren().addAll(SRID_PATH, SRID_BTN_CHOOSE);
+        HBox hBoxInnerTwo = new HBox(10);
+        hBoxInnerTwo.getChildren().addAll(SRID_WIDTH, SRID_HEIGHT);
+        HBox hBoxInnerThree = new HBox(10);
+        hBoxInnerThree.getChildren().addAll(SRID_MARGIN, SRID_PADDING);
+        vBox.getChildren().addAll(SRID_NAME, hBoxInnerOne, hBoxInnerTwo, hBoxInnerThree);
+        dialog.setGraphic(vBox);
+        dialog.getDialogPane().getButtonTypes().addAll(APPLY_SRID, CANCEL_SRID);
+        return dialog;
     }
 }
--- src/main/java/cn/kungreat/fxgamemap/RootController.java
+++ src/main/java/cn/kungreat/fxgamemap/RootController.java
@@ -29,16 +29,16 @@ import java.util.regex.Pattern;
 @Getter
 public class RootController implements Initializable {
 
-    private static final Dialog<String> worldDialog = BaseDialog.getDialog("世界地图", "请输入世界地图名称:", "是否需要添加世界地图层级"
+    private static final Dialog<String> WORLD_DIALOG = BaseDialog.getDialog("世界地图", "请输入世界地图名称:", "是否需要添加世界地图层级"
             , BaseDialog.TEXT_WORLD, BaseDialog.APPLY_WORLD, BaseDialog.CANCEL_WORLD);
 
-    private static final Dialog<String> areaDialog = BaseDialog.getDialog("区域地图", "请输入区域地图信息:", "是否需要添加区域地图层级"
+    private static final Dialog<String> AREA_DIALOG = BaseDialog.getDialog("区域地图", "请输入区域地图信息:", "是否需要添加区域地图层级"
             , BaseDialog.getAreaRectangular(), BaseDialog.APPLY_AREA, BaseDialog.CANCEL_AREA);
 
-    private static final Dialog<String> mapDialog = BaseDialog.getDialog("分块地图", "请输入分块地图信息:", "是否需要添加分块地图"
+    private static final Dialog<String> MAP_DIALOG = BaseDialog.getDialog("分块地图", "请输入分块地图信息:", "是否需要添加分块地图"
             , BaseDialog.getMapRectangular(), BaseDialog.APPLY_MAP, BaseDialog.CANCEL_MAP);
 
-    private static final Dialog<String> linkMapBookDialog = BaseDialog.getDialog("使用说明", "地图完整的使用说明",
+    private static final Dialog<String> LINK_MAP_BOOK_DIALOG = BaseDialog.getDialog("使用说明", "地图完整的使用说明",
             """
                     1.世界地图是一个完整游戏地图的概念
                     2.区域地图是世界地图的子级.是一个区域的概念
@@ -46,6 +46,9 @@ public class RootController implements Initializable {
                         游戏过大时不会把全部地图信息存在一个图片里
                     """
             , null, ButtonType.CLOSE);
+
+    private static final Dialog<String> SEGMENT_RESOURCE_IMAGES_DIALOG = BaseDialog.getSegmentResourceImagesDialog();
+
     @FXML
     private HBox topHBox;
     @FXML
@@ -74,9 +77,6 @@ public class RootController implements Initializable {
         treeView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         treeView.prefHeightProperty().bind(stackPaneLeft.heightProperty().subtract(stackPaneLeftHBox.heightProperty()));
         treeView.setContextMenu(getTreeContextMenu());
-        worldDialog.setOnShowing(event -> BaseDialog.TEXT_WORLD.clear());
-        areaDialog.setOnShowing(event -> BaseDialog.TEXT_AREA.clear());
-        mapDialog.setOnShowing(event -> BaseDialog.TEXT_MAP.clear());
         tabPaneRight.prefHeightProperty().bind(tabPaneRightVbox.heightProperty().subtract(tabPaneRightHBox.heightProperty()));
         addTreeEvent();
     }
@@ -85,20 +85,23 @@ public class RootController implements Initializable {
         ContextMenu contextMenu = new ContextMenu();
         MenuItem menuWorld = new MenuItem("add world");
         menuWorld.setGraphic(new FontIcon("fas-globe"));
-        menuWorld.setOnAction(event -> worldDialog.showAndWait());
+        menuWorld.setOnAction(event -> WORLD_DIALOG.showAndWait());
         MenuItem menuArea = new MenuItem("add Area");
         menuArea.setGraphic(new FontIcon("fas-chart-area"));
-        menuArea.setOnAction(event -> areaDialog.showAndWait());
+        menuArea.setOnAction(event -> AREA_DIALOG.showAndWait());
         MenuItem menuMap = new MenuItem("add Map");
         menuMap.setGraphic(new FontIcon("fas-map"));
-        menuMap.setOnAction(event -> mapDialog.showAndWait());
+        menuMap.setOnAction(event -> MAP_DIALOG.showAndWait());
         contextMenu.getItems().addAll(menuWorld, new SeparatorMenuItem(), menuArea, new SeparatorMenuItem(), menuMap);
         return contextMenu;
     }
 
     public void addTreeEvent() {
+        WORLD_DIALOG.setOnShowing(event -> BaseDialog.TEXT_WORLD.clear());
+        AREA_DIALOG.setOnShowing(event -> BaseDialog.TEXT_AREA.clear());
+        MAP_DIALOG.setOnShowing(event -> BaseDialog.TEXT_MAP.clear());
         //世界地图事件添加
-        Button applyWorld = (Button) worldDialog.getDialogPane().lookupButton(BaseDialog.APPLY_WORLD);
+        Button applyWorld = (Button) WORLD_DIALOG.getDialogPane().lookupButton(BaseDialog.APPLY_WORLD);
         applyWorld.setOnAction(event -> {
             String newWorld = BaseDialog.TEXT_WORLD.getText();
             if (!newWorld.isBlank()) {
@@ -111,7 +114,7 @@ public class RootController implements Initializable {
             }
         });
         //区域地图事件添加
-        Button applyArea = (Button) areaDialog.getDialogPane().lookupButton(BaseDialog.APPLY_AREA);
+        Button applyArea = (Button) AREA_DIALOG.getDialogPane().lookupButton(BaseDialog.APPLY_AREA);
         applyArea.setOnAction(event -> {
             String newArea = BaseDialog.TEXT_AREA.getText();
             String areaXText = BaseDialog.TEXT_AREAX.getText();
@@ -132,7 +135,7 @@ public class RootController implements Initializable {
             }
         });
         //地图事件添加
-        Button applyMap = (Button) mapDialog.getDialogPane().lookupButton(BaseDialog.APPLY_MAP);
+        Button applyMap = (Button) MAP_DIALOG.getDialogPane().lookupButton(BaseDialog.APPLY_MAP);
         applyMap.setOnAction(event -> {
             String title = BaseDialog.TEXT_MAP.getText();
             String mapWidth = BaseDialog.TEXT_MAP_WIDTH.getText();
@@ -156,7 +159,7 @@ public class RootController implements Initializable {
 
     @FXML
     public void showTreeBookDialog(MouseEvent mouseEvent) {
-        linkMapBookDialog.showAndWait();
+        LINK_MAP_BOOK_DIALOG.showAndWait();
     }
 
     @FXML
@@ -167,5 +170,10 @@ public class RootController implements Initializable {
             resourceTab.initTab();
             tabPaneRight.getTabs().add(resourceTab.getTab());
         }
+    }
+
+    @FXML
+    public void addSegmentResourceImg() {
+        SEGMENT_RESOURCE_IMAGES_DIALOG.showAndWait();
     }
 }

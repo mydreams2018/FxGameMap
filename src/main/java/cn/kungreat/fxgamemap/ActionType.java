6ed5a234cc059ebbc069c19d0ecac803a9710014
mydreams2018 <mydreams2018@outlook.com>
2024-08-26 14:22:14 +0800

    新增加功击范围

--- src/main/java/cn/kungreat/fxgamemap/ActionType.java
+++ src/main/java/cn/kungreat/fxgamemap/ActionType.java
@@ -5,15 +5,5 @@ package cn.kungreat.fxgamemap;
 * LONG_RANGE 远程
 * */
 public enum ActionType {
-    CLOSE_COMBAT(10), LONG_RANGE(800);
-
-    private final int actionDistance;
-
-    ActionType(int actionDistance) {
-        this.actionDistance = actionDistance;
-    }
-
-    public int getActionDistance() {
-        return actionDistance;
-    }
+    CLOSE_COMBAT, LONG_RANGE
 }

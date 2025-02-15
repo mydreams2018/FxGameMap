561cc474e8816006771ed615d59c10ea23d889f1
mydreams2018 <mydreams2018@outlook.com>
2025-02-15 12:11:14 +0800

    显示 加载数量优化

--- src/main/java/cn/kungreat/fxgamemap/custom/AreaMapShow.java
+++ src/main/java/cn/kungreat/fxgamemap/custom/AreaMapShow.java
@@ -173,8 +173,8 @@ public class AreaMapShow {
         xStartNumber = xStartNumber > 0 ? xStartNumber - 1 : 0;
         yStartNumber = yStartNumber > 0 ? yStartNumber - 1 : 0;
         //end
-        int xEndNumber = xStartNumber + 3 > treeAreaShow.getXNumber() ? treeAreaShow.getXNumber() : xStartNumber + 3;
-        int yEndNumber = yStartNumber + 3 > treeAreaShow.getYNumber() ? treeAreaShow.getYNumber() : yStartNumber + 3;
+        int xEndNumber = xStartNumber + 4 > treeAreaShow.getXNumber() ? treeAreaShow.getXNumber() : xStartNumber + 4;
+        int yEndNumber = yStartNumber + 4 > treeAreaShow.getYNumber() ? treeAreaShow.getYNumber() : yStartNumber + 4;
         for (int y = yStartNumber; y < yEndNumber; y++) {
             for (int x = xStartNumber; x < xEndNumber; x++) {
                 String[][] childrenPointName = treeAreaShow.getChildrenPointName();

29249da9ce1aa7903cbd496639bfb64ac4aab939
mydreams2018 <mydreams2018@outlook.com>
2025-03-08 00:53:08 +0800

    优化图层9

--- src/main/java/cn/kungreat/fxgamemap/RootController.java
+++ src/main/java/cn/kungreat/fxgamemap/RootController.java
@@ -459,7 +459,13 @@ public class RootController implements Initializable {
                                         //Height < 32 的情况
                                         int offsetX = Math.max((int) image.getWidth() - 48, 0);
                                         int offsetY = Math.max((int) image.getHeight() - 32, 0);
-                                        backgroundImages.add(new TreeGameMap.BackgroundImageData(ix * 48 - offsetX, iy * 32 - offsetY, imageName, ix, iy, 3));
+                                        boolean isTile = (image.getWidth() != 48 || image.getHeight() != 32) && (image.getWidth() != 48 * 2 || image.getHeight() != 32 * 2);
+                                        if (!isTile) {
+                                            //9是后面添加的上层地板层
+                                            backgroundImages.add(new TreeGameMap.BackgroundImageData(ix * 48 - offsetX, iy * 32 - offsetY, imageName, ix, iy, 9));
+                                        } else {
+                                            backgroundImages.add(new TreeGameMap.BackgroundImageData(ix * 48 - offsetX, iy * 32 - offsetY, imageName, ix, iy, 3));
+                                        }
                                     }
                                 }
                                 if (frontBlendData != null) {
@@ -513,41 +519,5 @@ public class RootController implements Initializable {
     @FXML
     public void changeShowPointLocks() {
         showPointLocks = !showPointLocks;
-    }
-
-    //修改frontTiles 标记为9
-    @FXML
-    public void changeFrontTileImageMark() {
-        TreeItem<Object> item = treeView.getFocusModel().getFocusedItem();
-        if (item != null && item.getValue() instanceof TreeWorld treeWorld) {
-            ArrayList<TreeArea> childrenArea = treeWorld.getChildrenArea();
-            if (childrenArea != null) {
-                for (TreeArea treeArea : childrenArea) {
-                    List<TreeGameMap> childrenMap = treeArea.getChildrenMap();
-                    if (childrenMap != null) {
-                        for (TreeGameMap treeGameMap : childrenMap) {
-                            List<TreeGameMap.BackgroundImageData> backgroundImages = treeGameMap.getBackgroundImages();
-                            if (backgroundImages != null) {
-                                for (TreeGameMap.BackgroundImageData backgroundImage : backgroundImages) {
-                                    if (backgroundImage != null) {
-                                        if (backgroundImage.getMirImageMark() == 3) {
-                                            backgroundImage.initImage(treeGameMap.getBackgroundImagePath());
-                                            Image image = backgroundImage.getImage();
-                                            boolean isTile = (image.getWidth() != 48 || image.getHeight() != 32) && (image.getWidth() != 48 * 2 || image.getHeight() != 32 * 2);
-                                            if (!isTile) {
-                                                backgroundImage.setMirImageMark(9);
-                                            }
-                                        }
-                                        backgroundImage.setLocatorX(null);//显示图层不用这个
-                                        backgroundImage.setLocatorY(null);//显示图层不用这个
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            System.out.println("changeFrontTileImageMark - end");
-        }
     }
 }

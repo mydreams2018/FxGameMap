54750aaa8fca4371507dbae7ea2e741ebd0fc762
mydreams2018 <mydreams2018@outlook.com>
2026-03-20 13:55:13 +0800

    特装地图 2

--- src/main/java/cn/kungreat/fxgamemap/RootController.java
+++ src/main/java/cn/kungreat/fxgamemap/RootController.java
@@ -109,7 +109,7 @@ public class RootController implements Initializable {
         addTreeEvent();
         addSegmentResourceImgEvent();
         addScrollPaneCenterEvent();
-        monsterChoiceBox.getItems().addAll("121", "122");
+        monsterChoiceBox.getItems().addAll("184", "122");
         npcChoiceBox.getItems().addAll("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "26", "27", "30", "33", "34", "99");
     }
 
